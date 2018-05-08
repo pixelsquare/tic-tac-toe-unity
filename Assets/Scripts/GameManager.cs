50afc65edc790cb92d8413227450211a1e76dde3
Anthony Ganzon <anthony_0205@yahoo.com>
2018-05-08 22:02:23 +0800

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,155 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour 
+{
+	public const int GRID_WIDTH = 3;
+	public const int GRID_HEIGHT = 3;
+
+	[SerializeField] private GameObject m_tile = null;
+	[SerializeField] private float m_tileOffset = 0.0f;
+
+	[SerializeField] private Color m_tileActiveColor = new Color();
+	[SerializeField] private Color m_tileHoverColor = new Color();
+
+	private int m_gridWidth = 0;
+	private int m_gridHeight = 0;
+
+	private Tile[,] m_tiles = null;
+	private int m_turnCount = 0;
+
+	public void Start()
+	{
+		initializeGrid();
+	}
+
+	private void initializeGrid(int p_width = GRID_WIDTH, int p_height = GRID_HEIGHT)
+	{
+		m_gridWidth = p_width;
+		m_gridHeight = p_height;
+
+		m_tiles = new Tile[m_gridWidth, m_gridHeight];
+
+		if(m_gridWidth % 2 == 1)
+		{
+			m_gridWidth = m_gridWidth - 1;	
+		}
+
+		if(m_gridHeight % 2 == 1)
+		{
+			m_gridHeight = m_gridHeight - 1;
+		}
+
+		m_gridWidth /= 2;
+		m_gridHeight /= 2;
+
+		int tileIdx = 0;
+		int tilesY = 0;
+
+		for(int y = m_gridHeight; y >= -m_gridHeight; y--)
+		{
+			int tilesX = 0;
+			for(int x = -m_gridWidth; x <= m_gridWidth; x++)
+			{
+				GameObject tile = (GameObject)Instantiate(m_tile, Vector3.zero, Quaternion.identity);
+				tile.name = string.Format("Tile_{0}", tileIdx);
+
+				Tile tileComponent = tile.GetComponent<Tile>();
+				if(null == tileComponent)
+				{
+					tileComponent = tile.AddComponent<Tile>();
+				}
+
+				tileComponent.setHoverColor(m_tileHoverColor);
+				tileComponent.setActiveColor(m_tileActiveColor);
+
+				tileComponent.setGridPosition(tilesX, tilesY);
+				tileComponent.addTileClickedEvent(onTileClickedEvent);
+
+				float tileWidth = tileComponent.getTextureWidth();
+				float tileHeight = tileComponent.getTextureHeight();
+
+				Transform tileT = tile.transform;
+				tileT.SetParent(transform, false);
+				tileT.position = new Vector3(tileWidth * x, tileHeight * y, 0.0f) * m_tileOffset;
+
+				m_tiles[tilesX, tilesY] = tileComponent;
+
+				tileIdx++;
+				tilesX++;
+			}
+
+			tilesY++;
+		}
+	}
+
+	private void onTileClickedEvent(Tile p_tile)
+	{
+		int turnCount = m_turnCount;
+
+		if(turnCount % 2 == 0)
+		{
+			p_tile.setTextureX();
+		}
+		else
+		{
+			p_tile.setTextureO();
+		}
+
+		Debug.Log(isGameOver(p_tile));
+
+		m_turnCount++;
+	}
+
+	private bool isGameOver(Tile p_tile)
+	{
+		bool retVal = false;
+
+		int gridXPos = p_tile.getGridX();
+		int gridYPos = p_tile.getGridY();
+
+		for(int i = 0; i < m_gridWidth; i++)
+		{
+			if(m_tiles[i, gridYPos].getTileValue() != p_tile.getTileValue())
+			{
+				break;
+			}
+
+			if(i == m_gridWidth)
+			{
+				retVal = true;
+			}
+		}
+
+		return retVal;
+	}
+
+	private List<Tile> getNeighborTiles(Tile p_tile)
+	{
+		List<Tile> retVal = new List<Tile>();
+
+		int gridXPos = p_tile.getGridX();
+		int gridYPos = p_tile.getGridY();
+
+		for(int x = gridXPos - 1; x <= gridXPos + 1; x++)
+		{
+			for(int y = gridYPos - 1; y <= gridYPos + 1; y++)
+			{
+				if(x < 0 || x >= m_gridWidth + 1 || y < 0 || y > m_gridHeight + 1)
+				{
+					continue;
+				}
+
+				if(x == gridXPos && y == gridYPos)
+				{
+					continue;
+				}
+
+				retVal.Add(m_tiles[x, y]);
+			}
+		}
+
+		return retVal;
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void OnTileClicked(Tile p_tile);
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Tile : MonoBehaviour 
+{
+	[SerializeField] private SpriteRenderer m_sprTileValue = null;
+	[SerializeField] private Sprite m_spriteX = null;
+	[SerializeField] private Sprite m_spriteO = null;
+
+	private GameObject m_gameobject = null;
+	private Transform m_transform = null;
+
+	private SpriteRenderer m_spriteRenderer = null;
+
+	private Texture2D m_texture = null;
+
+	private float m_textureWidth = 0.0f;
+	private float m_textureHeight = 0.0f;
+
+	private Vector3 m_textureSize = new Vector3();
+	private Vector3 m_textureExtents = new Vector3();
+
+	private Color m_originalColor = new Color();
+	private Color m_hoverColor = new Color();
+	private Color m_activeColor = new Color();
+
+	private bool m_isMouseOver = false;
+	private bool m_hasValue = false;
+
+	private int m_gridX = 0;
+	private int m_gridY = 0;
+
+	private int m_tileValue = -1;
+
+	private event OnTileClicked m_tileClickedEvent;
+
+	public void Awake()
+	{
+		m_gameobject = gameObject;
+		m_transform = transform;
+
+		m_spriteRenderer = GetComponent<SpriteRenderer>();
+		m_originalColor = m_spriteRenderer.color;
+
+		m_texture = m_spriteRenderer.sprite.texture;
+
+		if(null != m_texture)
+		{
+			m_textureWidth = m_texture.width;
+			m_textureHeight = m_texture.height;
+
+			m_textureSize = new Vector3(m_textureWidth, m_textureHeight, 0.0f);
+			m_textureExtents = m_textureSize / 2.0f;
+		}
+	}
+
+	public void OnMouseOver()
+	{
+		if(m_hasValue)
+		{
+			return;
+		}
+
+		m_spriteRenderer.color = m_hoverColor;
+		m_isMouseOver = true;
+	}
+
+	public void OnMouseExit()
+	{
+		if(m_hasValue)
+		{
+			return;
+		}
+
+		m_spriteRenderer.color = m_originalColor;
+		m_isMouseOver = false;
+	}
+
+	public void OnMouseDown()
+	{
+		if(m_hasValue)
+		{
+			return;
+		}
+		m_spriteRenderer.color = m_activeColor;
+	}
+
+	public void OnMouseUp()
+	{
+		if(!m_isMouseOver || m_hasValue)
+		{
+			return;
+		}
+
+		if(null != m_tileClickedEvent)
+		{
+			m_tileClickedEvent(this);
+		}
+	}
+
+	public float getTextureWidth()
+	{
+		return m_textureWidth;
+	}
+
+	public float getTextureHeight()
+	{
+		return m_textureHeight;
+	}
+
+	public Texture2D getTexture()
+	{
+		return m_texture;
+	}
+
+	public Vector3 getSize()
+	{
+		return m_textureSize;
+	}
+
+	public Vector3 getCenter()
+	{
+		return m_transform.position;
+	}
+
+	public Vector3 getExtents()
+	{
+		return m_textureExtents;
+	}
+
+	public Vector3 getMin()
+	{
+		return m_transform.position - m_textureExtents;
+	}
+
+	public Vector3 getMax()
+	{
+		return m_transform.position + m_textureExtents;
+	}
+
+	public int getTileValue()
+	{
+		return m_tileValue;
+	}
+
+	public void setHoverColor(Color p_hoverColor)
+	{
+		m_hoverColor = p_hoverColor;
+	}
+
+	public void setActiveColor(Color p_activeColor)
+	{
+		m_activeColor = p_activeColor;
+	}
+
+	public void setGridPosition(int p_gridX, int p_gridY)
+	{
+		m_gridX = p_gridX;
+		m_gridY = p_gridY;
+	}
+
+	public int getGridX()
+	{
+		return m_gridX;
+	}
+
+	public int getGridY()
+	{
+		return m_gridY;
+	}
+
+	public void addTileClickedEvent(OnTileClicked p_tileClicked)
+	{
+		m_tileClickedEvent += p_tileClicked;
+	}
+
+	public void removeTileClickedEvent(OnTileClicked p_tileClicked)
+	{
+		m_tileClickedEvent -= p_tileClicked;
+	}
+
+	public void setTextureX()
+	{
+		m_spriteRenderer.color = m_originalColor;
+		m_sprTileValue.sprite = m_spriteX;
+		m_hasValue = true;
+		m_tileValue = 1;
+	}
+
+	public void setTextureO()
+	{
+		m_spriteRenderer.color = m_originalColor;
+		m_sprTileValue.sprite = m_spriteO;
+		m_hasValue = true;
+		m_tileValue = 0;
+	}
+}
